f4000f96d07990804481293650e0a2e7f6e653f8
Timon Schmid <timon.schmid@bluewin.ch>
2024-02-13 20:11:55 +0100

    add improvement endpoint

--- src/main/java/ch/better/u/GreetingResource.java
+++ src/main/java/ch/better/u/GreetingResource.java
@@ -5,12 +5,29 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/")
 public class GreetingResource {
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
+
+    @GET
+    @Path("/improvements")
+    @Produces(MediaType.TEXT_PLAIN)
+    public List<String> getImprovementList(){
+        List<String> improvements = new ArrayList<>();
+        improvements.add("Brush teeth");
+        improvements.add("Clean kitchen");
+        return improvements;
+    }
+
+
+
 }
